ea1f8ec3027815b42903b860b52107430947fda3
quan1222009 <12467ygjdjebieeee@gmail.com>
2025-12-02 14:13:43 +0700

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,11 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+const FriendRequestSchema = new mongoose.Schema({
+  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now }
+}, { _id: false });
 
 const UserSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, sparse: true },
   password: String,
   avatar: String,
-  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  // incoming friend requests
+  friendRequests: [FriendRequestSchema],
+  // optionally track outgoing
+  friendRequestsSent: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
 }, { timestamps: true });
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
